de55c7d1d77359090fb1acd20458c641f4e82b2f
Timothy Short <timothyjames.short@gmail.com>
2017-06-28 20:52:51 -0400

    Update NegativePaymentException.java

--- src/com/TrainingRite/Exceptions/NegativePaymentException.java
+++ src/com/TrainingRite/Exceptions/NegativePaymentException.java
@@ -2,6 +2,8 @@ package com.TrainingRite.Exceptions;
 
 public class NegativePaymentException extends Exception {
 	
+	// This is a user defined exception that will throw an exception when there is a negative number
+	
 	double amount;
 	public NegativePaymentException(double amount) {
 		this.amount = amount;
